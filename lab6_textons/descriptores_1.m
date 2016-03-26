9541c15b5a473c7777ccfbc850d308114364e438
diegomartinez33 <da.martinez33@uniandes.edu.co>
2016-03-26 18:30:14 -0500

    Create descriptores_1.m

---
+++ lab6_textons/descriptores_1.m
@@ -0,0 +1,30 @@
+%%Lab Textones
+%% Diccionario de textones para cada imagen
+addpath('lib');
+clear all;close all;clc;
+
+% create filter bank
+[fb] = fbCreate;
+% numero de textones en diccionario
+k = 50;
+%Carpeta de base de datos de entrenamiento
+dirtrain = dir(fullfile(cd,'textures','train','*.jpg'));
+
+%lecura de imágenes
+TXT=struct('name',[],'image',[],'map',[],'textons',[]);
+
+cont=1;
+for i=1:13
+    for j=1:4
+        TXT(cont).image = imread(fullfile(cd,'textures','train',...
+            dirtrain(j+(30*i)).name));
+        TXT(cont).name = dirtrain(j+(30*i)).name;
+        % diccionario de textones
+        im=TXT(cont).image;
+        [map,textons] = computeTextons(fbRun(fb,im),k);
+        TXT(cont).map=map;
+        TXT(cont).textons=textons;
+        cont=cont+1;
+        cont
+    end
+end
